1b10074b20de194895b96ce5ff07acfc7ae9c528
trainer <trainer@mac.local>
2015-12-02 19:09:06 +0000

    adding a questions controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,21 @@
+class Question
+	attr_accessor :header
+	
+end
+
+
 class QuestionsController < ApplicationController
+	def index
+		@questions = []
+		
+		question1 = Question.new
+		question1.header = "What is my name?"
+
+		question2 = Question.new
+		question2.header = "Why are we here?"
+
+		@questions << question1
+		@questions << question2
+		# @questions = ['Is Pluto a planet?', 'Why am I here?', 'Who am I?']
+	end
 end
