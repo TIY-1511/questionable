477469a6a9d8daa4df3b111cacb8a2758ce4f277
trainer <trainer@mac.local>
2015-12-02 19:39:02 +0000

    adding the model

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,21 +1,6 @@
-class Question
-	attr_accessor :header
-	
-end
-
-
 class QuestionsController < ApplicationController
 	def index
-		@questions = []
-		
-		question1 = Question.new
-		question1.header = "What is my name?"
-
-		question2 = Question.new
-		question2.header = "Why are we here?"
-
-		@questions << question1
-		@questions << question2
+		@questions = Question.all
 		# @questions = ['Is Pluto a planet?', 'Why am I here?', 'Who am I?']
 	end
 end
