36e0d7c38a4bbc0147b17244a4bcc29c80ea5a79
trainer <trainer@mac.local>
2015-12-09 18:52:32 +0000

    completing 10

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,9 @@
 class QuestionsController < ApplicationController
-	def index
-		@questions = Question.all
-		# @questions = ['Is Pluto a planet?', 'Why am I here?', 'Who am I?']
-	end
+    def index
+        @questions = Question.all
+    end
+
+    def show
+        @question = Question.find(params[:id])
+    end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+	def format_body(question)
+		if question.body.present?
+		    # note you can call a helper from a helper
+		    truncate(question.body, length: 20, separator: ' ')
+		else
+		    '<i>n/a</i>'
+		end
+	end
 end
