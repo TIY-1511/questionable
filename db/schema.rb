9db211443a035d15da2421e4f89cdd51c8990b68
Daryn Holmes <daryn.holmes@net-a-porter.com>
2015-12-09 18:40:51 +0000

    adding body

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151202191025) do
+ActiveRecord::Schema.define(version: 20151207200120) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
